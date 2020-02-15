9f638666f661801bdc01e070ec05928149f475cc
resilient-coders24 <resilient-coders24@resilient-coders24s-MacBook-Pro.local>
2020-02-15 18:37:13 -0500

    week5 these are my code war challenges that are helping me better understand my javascript

---
+++ mainthree.js
@@ -0,0 +1,5 @@
+//Very simple, given a number, find its opposite.
+//ANSWER//
+function opposite(number) {
+  return (-number)
+}
---
+++ maintwo.js
@@ -0,0 +1,9 @@
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//ANSWER//
+function makeNegative(n) {
+if (n<0){
+return (n)
+}else{
+return (n * -1)
+}
+}

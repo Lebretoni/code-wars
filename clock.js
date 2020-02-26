ff8adce7bc95c613ef19f81e7c389b2c1f7a73d9
Lebretoni <60005137+Lebretoni@users.noreply.github.com>
2020-02-25 21:17:44 -0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,6 @@
+//Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+//Your task is to make 'Past' function which returns time converted to milliseconds.
+//ANSWER
+function past(h, m, s){
+return 3600000*h+60000*m+1000*s
+}
